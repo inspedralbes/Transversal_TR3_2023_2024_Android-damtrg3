7c54d2996e88dc190ffc606f19531a706bf37294
C0ural1 <albertcoralm@gmail.com>
2024-03-21 09:35:39 +0100

    Nova branch per canvis

--- core/src/com/mygdx/game/screens/OptionsScreen.java
+++ core/src/com/mygdx/game/screens/OptionsScreen.java
@@ -155,6 +155,7 @@ public class OptionsScreen implements Screen {
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height, true);
+
     }
 
     @Override
