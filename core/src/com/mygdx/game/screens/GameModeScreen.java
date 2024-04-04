92f9f3144c72bf5771980dabd5b353b7c388955e
PanDyyG <a22angcamrui@inspedralbes.cat>
2024-04-04 09:29:06 +0200

    Modificacio GameModeScreen i RankingSreen

--- core/src/com/mygdx/game/screens/GameModeScreen.java
+++ core/src/com/mygdx/game/screens/GameModeScreen.java
@@ -87,6 +87,7 @@ public class GameModeScreen implements Screen {
         // Crear un TextButton con texto
         TextButton SoloButton = new TextButton("Inidividual", skin);
         TextButton MultiButton = new TextButton("Multijugador", skin);
+        TextButton Ranking = new TextButton("Ranking", skin);
 
         SoloButton.addListener(new ClickListener() {
             @Override
@@ -113,6 +114,19 @@ public class GameModeScreen implements Screen {
                 });
             }
         });
+        Ranking.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Aquí va el código que se ejecutará cuando se haga clic en el TextButton
+                Gdx.app.postRunnable(new Runnable() {
+                    @Override
+                    public void run() {
+                        game.setScreen(new RankingSreen(game));
+                    }
+                });
+            }
+        });
+
 
         // Añadir la imagen (dentro del contenedor) como actor al botón
         SoloButton.add(imageContainer);
@@ -242,6 +256,8 @@ public class GameModeScreen implements Screen {
         contentTable.add(SoloButton).left().padBottom(10);
         contentTable.row();
         contentTable.add(MultiButton).left().padBottom(10);
+        contentTable.row();
+        contentTable.add(Ranking).left().padBottom(10);
 
 
 
--- core/src/com/mygdx/game/screens/RankingSreen.java
+++ core/src/com/mygdx/game/screens/RankingSreen.java
@@ -1,4 +1,111 @@
 package com.mygdx.game.screens;
 
-public class RankingSreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.mygdx.game.Projecte3;
+import com.mygdx.game.helpers.AssetManager;
+import com.mygdx.game.utils.Settings;
+
+public class RankingSreen implements Screen {
+    private Projecte3 game;
+    private Stage stage;
+    private TextButton backButton;
+    private Batch batch;
+    public RankingSreen(Projecte3 game) {
+        this.game = game;
+
+
+    }
+
+    @Override
+    public void show() {
+        // Crear un nuevo SpriteBatch
+        batch = new SpriteBatch();
+
+        stage = new Stage(); // Initialize stage first
+        Gdx.input.setInputProcessor(stage); // Set InputProcessor after stage initialization
+
+        Table wrapperTable = new Table(); // Table para envolver los campos
+        wrapperTable.setSize(500, 600); // Establece el tamaño deseado para la tabla
+        wrapperTable.setPosition((Gdx.graphics.getWidth() - wrapperTable.getWidth()) / 2, (Gdx.graphics.getHeight() - wrapperTable.getHeight()) / 2); // Centra la tabla en la pantalla
+
+        Texture backgroundTexture = new Texture(Gdx.files.internal("frame6.png"));
+        TextureRegionDrawable backgroundDrawable = new TextureRegionDrawable(new TextureRegion(backgroundTexture));
+        wrapperTable.setBackground(backgroundDrawable);
+
+        Table contentTable = new Table(); // Table para los campos de entrada y botones
+        contentTable.pad(20); // Agrega un relleno de 20 píxeles alrededor del contenido
+
+        backButton = new TextButton("Back", AssetManager.lava_skin);
+
+        // Agrega los actores al contentTable
+        contentTable.add(backButton).pad(10);
+        contentTable.row();
+
+        wrapperTable.add(contentTable).center(); // Agrega el Table de contenido dentro del Table de envoltura
+
+        stage.addActor(wrapperTable); // Agrega el Table de envoltura al Stage
+
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new GameModeScreen(game));
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(AssetManager.menu_bg2, 0, 0, Settings.SCREEN_WIDTH, Settings.SCREEN_HEIGHT);
+        batch.end();
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
